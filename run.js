bae25b31e209bf8ee5a01f4254c0e03d76076edf
rohaneee <rohaneee@gmail.com>
2017-09-15 01:46:26 +0100

    First checkin using source tree

--- run.js
+++ run.js
@@ -2,5 +2,6 @@ define(["app", "angular"], function(module, angular){
     var run = function($rootScope){
 
     };
+	// Starting point of app source branch
     angular.module(module.HOME).run(["$rootScope", run]);
 });
