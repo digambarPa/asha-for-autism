c1dc0bba3b19e4b8e1ee97d38f35acefe9f91306
rohaneee <rohaneee@gmail.com>
2017-09-15 02:14:39 +0100

    removed comment

--- run.js
+++ run.js
@@ -2,6 +2,5 @@ define(["app", "angular"], function(module, angular){
     var run = function($rootScope){
 
     };
-	// Starting point of app source branch
     angular.module(module.HOME).run(["$rootScope", run]);
 });
